b7f3c66589ef7d623f534367ae5c25c93a2a9567
compudefierro <jorgeeldri@gmail.com>
2023-05-04 20:02:55 -0300

    Ejercicio nro 1 realizado

---
+++ ejercicio1.js
@@ -0,0 +1,11 @@
+// Ejercicio 1: Creando variables
+// Crea variables para guardar los siguientes datos de una persona
+
+let nombre = "Jorge Osvaldo";
+let apellido = "Dri";
+let edad = 42;
+let ciudad = "Paso de los Libres";
+
+console.log(`${nombre} ${apellido}`); // Multi-line template literals
+console.log("Edad: " + edad + " años.");
+console.log(`Reside en la ciudad de ${ciudad}.`);
