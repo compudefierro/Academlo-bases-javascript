b0e7df3b8f03c69e58ee6895b9679eefb64d1e07
compudefierro <jorgeeldri@gmail.com>
2023-05-05 11:53:57 -0300

    Ejercicio 02 listas, objetos, date

---
+++ OB/02_Ejercicio.js
@@ -0,0 +1,21 @@
+/*
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+
+let lista = [
+  "Jorge",
+  38,
+  true,
+  Date(8, 16, 1980),
+  {
+    titulo: "Saga La torre oscura",
+    autor: "Stephen King",
+    fecha: Date(30, 8, 1999),
+    url: "https://es.wikipedia.org/wiki/La_Torre_Oscura",
+  },
+];
